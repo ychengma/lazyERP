a15b1105a473b59091d8d8da6dc0132e1d2f8bae
mathieu <ychengma@gmail.com>
2024-06-28 00:37:50 +0200

    add router

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,7 +6,7 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css' 
 
 import App from './App.vue'
-import router from './router'
+import router from '@/router'
 
 const app = createApp(App)
 
